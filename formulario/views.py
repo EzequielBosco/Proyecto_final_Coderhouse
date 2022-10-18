9dd8a0756d6b2a3b24f8501cb21b5a2852cebb7a
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-18 16:26:39 -0300

    Agrego template index

--- formulario/views.py
+++ formulario/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "formulario/saludar.html")
+def index1(request):
+    return render(request, "formulario/index.html")
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from formulario.views import index
+from formulario.views import index, index1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formulario/', index),
+    path('prueba/', index1),
 ]
