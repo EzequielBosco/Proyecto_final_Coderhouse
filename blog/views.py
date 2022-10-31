e50f6948ffeb369ef4f10d0a0e95408b704a5e79
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-31 13:37:11 -0300

    creo admin

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Configuracion
 
 def Home(request):
-    return render (request, "blog/index.html")
+    configuracion = Configuracion.objects.first()
+    return render (request, "blog/index.html", {"configuracion":configuracion})
 
 def About(request):
     return render (request, "blog/about.html")
