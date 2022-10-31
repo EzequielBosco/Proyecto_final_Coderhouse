bc304b892b3a7fce167cf9208ca4cb24d7ff5c3a
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-31 14:03:31 -0300

    agrego configuracion y cambio idioma settings

--- blog/models.py
+++ blog/models.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Configuracion(models.Model):
     nombre_blog = models.CharField(max_length=14)
+    titulo_blog = models.CharField(max_length=20)
+    subtitulo_blog = models.CharField(max_length=30)
+    desarrollado_por = models.CharField(max_length=30)
