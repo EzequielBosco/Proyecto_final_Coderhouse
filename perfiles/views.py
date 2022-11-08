73df4e9fa22ff7e2610e5454a60df67d97881d54
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-11-07 21:31:47 -0300

    actualizo url actualizar perfil, creo carpeta registration

--- perfiles/views.py
+++ perfiles/views.py
@@ -32,7 +32,7 @@ class BlogLogout(LogoutView):
 class BlogSignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("blog-login")
-    template_name = "perfiles/signup.html"
+    template_name = "registration/signup.html"
 
 class ProfileUpdate(UpdateView):
     model = User
