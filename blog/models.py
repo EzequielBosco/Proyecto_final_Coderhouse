a897a030c65d2e158b1e29c7048de6e5a4af00cc
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-11-09 02:04:46 -0300

    Agrego imagenes para fondos, collectstatics

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,8 @@ class ConfigIndex(models.Model):
     
 class ConfigAbout(models.Model):
     titulo = models.CharField(max_length=20)
-    sub_titulo = models.CharField(max_length=30)
-    
+    sub_titulo = models.CharField(max_length=200)
+
 class ConfigContact(models.Model):
     titulo = models.CharField(max_length=20)
-    sub_titulo = models.CharField(max_length=30)
+    sub_titulo = models.CharField(max_length=50)
